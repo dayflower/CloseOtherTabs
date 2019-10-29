a312579bc3b77847c81e3333ecd18edf4dfe1927
dayflower <daydream.trippers@gmail.com>
2019-10-29 11:43:11 +0900

    Add `currentWindow` parameter for `chrome.tabs.query()`, thanks to foo

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 function iconClick(tab) {
-	chrome.tabs.query({"active": false, "pinned": false}, function(listOfTabs) {
+	chrome.tabs.query({"currentWindow": true, "active": false, "pinned": false}, function(listOfTabs) {
 		for (var i=0; i<listOfTabs.length; i++) {
 			chrome.tabs.remove(listOfTabs[i].id);	
 		}
